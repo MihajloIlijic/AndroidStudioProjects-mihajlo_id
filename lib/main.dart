a51b65bbabf71c5361bc7877456fedabd06c57cc
Mihajlo Ilijic <mihajlo.ilijic@edu.fh-joanneum.at>
2024-05-25 15:01:17 +0200

    commit 25.05.24

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyProject extends StatelessWidget {
               ),
             ),
             Text(
-              'Mihajlo Ilijic',
+              'Mihajlo Iliic',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Colors.amberAccent,
